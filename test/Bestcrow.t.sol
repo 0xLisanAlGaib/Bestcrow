1715b2456ce363e622b13f26c118636ca43d4dc8
0xLisanAlGaib <0xlisanalgaib@gmail.com>
2024-11-13 16:06:44 -0600

    Added testing for accepting escrow

--- test/Bestcrow.t.sol
+++ test/Bestcrow.t.sol
@@ -6,6 +6,8 @@ import {Bestcrow} from "../src/Bestcrow.sol";
 import {MockERC20} from "./mocks/MockERC20.sol";
 
 contract BestcrowTest is Test {
+    event EscrowAccepted(uint256 indexed escrowId, address indexed receiver);
+
     Bestcrow public bestcrow;
     MockERC20 public token;
 
@@ -14,6 +16,9 @@ contract BestcrowTest is Test {
     uint256 public constant MILESTONES = 4;
     uint256 public constant DAYS_TO_EXPIRY = 30;
 
+    address public receiver = makeAddr("receiver");
+    uint256 public escrowId; // We'll use this for setup
+
     function setUp() public {
         // Deploy contracts
         bestcrow = new Bestcrow();
@@ -21,24 +26,34 @@ contract BestcrowTest is Test {
 
         // Setup depositor with ETH and tokens
         vm.deal(depositor, 10 ether);
+        vm.deal(receiver, 10 ether); // Give receiver some ETH too
         token.mint(depositor, 10 ether);
+        token.mint(receiver, 10 ether); // Give receiver some tokens too
 
         // Approve tokens for depositor
         vm.prank(depositor);
         token.approve(address(bestcrow), type(uint256).max);
+
+        // Approve tokens for receiver
+        vm.prank(receiver);
+        token.approve(address(bestcrow), type(uint256).max);
+    }
+
+    function _createEthEscrow() internal returns (uint256) {
+        vm.prank(depositor);
+        return bestcrow.createEscrow{value: AMOUNT}(address(0), AMOUNT, MILESTONES, DAYS_TO_EXPIRY);
+    }
+
+    function _createTokenEscrow() internal returns (uint256) {
+        vm.prank(depositor);
+        return bestcrow.createEscrow(address(token), AMOUNT, MILESTONES, DAYS_TO_EXPIRY);
     }
 
     function test_createEscrowWithEth() public {
-        // Switch to depositor context
         vm.prank(depositor);
 
         // Create escrow with ETH
-        uint256 escrowId = bestcrow.createEscrow{value: AMOUNT}(
-            address(0), // address(0) indicates ETH
-            AMOUNT,
-            MILESTONES,
-            DAYS_TO_EXPIRY
-        );
+        escrowId = bestcrow.createEscrow{value: AMOUNT}(address(0), AMOUNT, MILESTONES, DAYS_TO_EXPIRY);
 
         // Get the created escrow
         (
@@ -71,7 +86,7 @@ contract BestcrowTest is Test {
         vm.prank(depositor);
 
         // Create escrow with ERC20 token
-        uint256 escrowId = bestcrow.createEscrow(address(token), AMOUNT, MILESTONES, DAYS_TO_EXPIRY);
+        escrowId = bestcrow.createEscrow(address(token), AMOUNT, MILESTONES, DAYS_TO_EXPIRY);
 
         // Get the created escrow
         (
@@ -123,5 +138,106 @@ contract BestcrowTest is Test {
         vm.prank(depositor);
         bestcrow.createEscrow{value: 0.5 ether}( // Sending wrong amount
         address(0), AMOUNT, MILESTONES, DAYS_TO_EXPIRY);
+    }
+
+    function test_acceptEscrowWithEth() public {
+        uint256 _escrowId = _createEthEscrow();
+        uint256 receiverInitialBalance = receiver.balance;
+
+        vm.prank(receiver);
+        bestcrow.acceptEscrow{value: AMOUNT}(_escrowId);
+
+        // Get the escrow details
+        (address _depositor, address _receiver,,,,,,,) = bestcrow.escrows(_escrowId);
+
+        // Assert the escrow state
+        assertEq(_receiver, receiver);
+        assertEq(_depositor, depositor);
+
+        // Assert balances
+        assertEq(address(bestcrow).balance, AMOUNT * 2); // Original deposit + collateral
+        assertEq(receiver.balance, receiverInitialBalance - AMOUNT); // Should have sent collateral
+    }
+
+    function test_acceptEscrowWithToken() public {
+        escrowId = _createTokenEscrow();
+        uint256 receiverInitialBalance = token.balanceOf(receiver);
+
+        vm.prank(receiver);
+        bestcrow.acceptEscrow(escrowId);
+
+        // Get the escrow details
+        (address _depositor, address _receiver,,,,,,,) = bestcrow.escrows(escrowId);
+
+        // Assert the escrow state
+        assertEq(_receiver, receiver);
+        assertEq(_depositor, depositor);
+
+        // Assert balances
+        assertEq(token.balanceOf(address(bestcrow)), AMOUNT * 2); // Original deposit + collateral
+        assertEq(token.balanceOf(receiver), receiverInitialBalance - AMOUNT); // Should have sent collateral
+    }
+
+    function testFail_acceptExpiredEscrow() public {
+        uint256 _escrowId = _createEthEscrow();
+
+        // Fast forward past expiry
+        vm.warp(block.timestamp + (DAYS_TO_EXPIRY * 1 days) + 1);
+
+        vm.prank(receiver);
+        bestcrow.acceptEscrow{value: AMOUNT}(_escrowId);
+    }
+
+    function testFail_acceptAlreadyAcceptedEscrow() public {
+        escrowId = _createEthEscrow();
+
+        // First acceptance
+        vm.prank(receiver);
+        bestcrow.acceptEscrow{value: AMOUNT}(escrowId);
+
+        // Create another receiver
+        address receiver2 = makeAddr("receiver2");
+        vm.deal(receiver2, AMOUNT);
+
+        // Try to accept again
+        vm.prank(receiver2);
+        bestcrow.acceptEscrow{value: AMOUNT}(escrowId);
+    }
+
+    function testFail_acceptEscrowAsDepositor() public {
+        escrowId = _createEthEscrow();
+
+        vm.prank(depositor);
+        bestcrow.acceptEscrow{value: AMOUNT}(escrowId);
+    }
+
+    function testFail_acceptEscrowWithIncorrectEthAmount() public {
+        escrowId = _createEthEscrow();
+
+        vm.prank(receiver);
+        bestcrow.acceptEscrow{value: AMOUNT - 0.1 ether}(escrowId);
+    }
+
+    function testFail_acceptInactiveEscrow() public {
+        escrowId = _createEthEscrow();
+
+        // Complete the escrow somehow (you might need to add a function for this in your contract)
+        // For now, let's assume it's expired and refunded
+        vm.warp(block.timestamp + (DAYS_TO_EXPIRY * 1 days) + 1);
+        bestcrow.refundExpiredEscrow(escrowId);
+
+        vm.prank(receiver);
+        bestcrow.acceptEscrow{value: AMOUNT}(escrowId);
+    }
+
+    function test_acceptEscrowEmitsEvent() public {
+        escrowId = _createEthEscrow();
+
+        vm.prank(receiver);
+
+        vm.expectEmit(true, true, false, false);
+        emit EscrowAccepted(escrowId, receiver);
+
+        bestcrow.acceptEscrow{value: AMOUNT}(escrowId);
     }
 }
