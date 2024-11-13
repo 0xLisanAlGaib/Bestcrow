6116357630bf193e25cd33cf25efc8b663d2d341
0xLisanAlGaib <0xlisanalgaib@gmail.com>
2024-11-13 11:24:34 -0600

    Created test file

---
+++ test/Bestcrow.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import {Bestcrow} from "../src/Bestcrow.sol";
+
+contract BestcrowTest is Test {
+    Bestcrow public bestcrow;
+
+    function test_createEscrow() public {
+    }
+}
